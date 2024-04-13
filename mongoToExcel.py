699375fcef2fff96819253fd3c06f83059e8835b
VyacheslavKuchumov <Kuchumov76@mail.ru>
2024-04-13 16:38:24 +0500

    nice imba that creates excel doc and pie chart

--- mongoToExcel.py
+++ mongoToExcel.py
@@ -8,9 +8,9 @@ df = pd.DataFrame(data)
 
 file_name = 'jobs.xlsx'
 
-counts = df['fieldOfExp'].value_counts()
 
-print(counts)
+
+
 
 try:
     df.to_excel(file_name)
@@ -18,5 +18,19 @@ try:
 except Exception as e:
     print("Couldn't make an Excel file BRUHHHHHHHHHHHH", e)
 
+
+
+value_counts = df['fieldOfExp'].value_counts()
+import matplotlib.pyplot as plt
+# Plot as a pie chart
+plt.figure(figsize=(8, 6))
+patches, _ = plt.pie(value_counts, labels=None, startangle=140)
+
+# Add legend for each category with percentages
+plt.legend(handles=patches, labels=[f'{label} ({value_counts[label]}) - {value_counts[label]/len(df)*100:.1f}%' for label in value_counts.index], loc="center left", bbox_to_anchor=(1, 0.5))
+
+plt.title('Distribution of Categories')
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.show()
 
 client.close()
